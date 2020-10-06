ca6597600afcd939637682e8244606e80b13fe34
Rehoboth Okorie <65668510+rehoboth23@users.noreply.github.com>
2020-10-06 01:29:01 -0400

    Update copy2clip.js

--- static/shorten/js/copy2clip.js
+++ static/shorten/js/copy2clip.js
@@ -1,13 +1,15 @@
 $(document).ready(function myFunction() {
   $("#copyBtna").click(function () {
-    var copyText = document.getElementById("shortUrla");
+    var copyText = document.getElementById("shortUrla")
+    console.log(copyText)
     console.log("A")
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
   })
   $("#copyBtnb").click(function () {
-    var copyText = document.getElementById("shortUrlb");
+    var copyText = document.getElementById("shortUrlb")
+    console.log(copyText)
     console.log("B")
     copyText.select();
     copyText.setSelectionRange(0, 99999)
